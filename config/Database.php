5b51d20e4ca23c7a62ef05fe6081b1d3591e5995
BugCasterSenpai <69622075+BugCasterSenpai@users.noreply.github.com>
2022-09-28 23:18:03 +0800

    Pushed Plan update and delete

--- config/Database.php
+++ config/Database.php
@@ -1,29 +1,21 @@
-<?php
-    class Database 
-    {
-        // DB Params
-        private $host = 'localhost:3307';
-        private $db_name = 'gstech_bms_db';
-        private $username = 'root';
-        private $password = '';
-        private $conn;
+<?php 
+  class Database {
+    private $host = 'localhost';
+    private $db_name = 'gstech_bms_db_fk';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
 
-        // DB Connect
-        public function connect()
-        {
-            $this->conn = null;
+    public function connect() {
+      $this->conn = null;
 
-            try
-            {
-                $this->conn = new PDO('mysql:host=' .$this->host. ';dbname=' .$this->db_name, $this->username, $this->password);
+      try { 
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
 
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            catch (PDOException $e)
-            {
-                echo 'Connection Error: '.$e->getMessage();
-            }
-
-            return $this->conn;
-        }
+      return $this->conn;
     }
+  }
