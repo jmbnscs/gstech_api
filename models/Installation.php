68ef8308361ddbe432c6874d1e587aa45bc84c51
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2022-10-01 23:24:26 +0800

    add installation create and read.

--- models/Installation.php
+++ models/Installation.php
@@ -19,15 +19,65 @@
         }
 
         # Create Post
-        public function create()
+        public function create ()
         {
+            // Clean Data
+            $this->install_type_id = htmlspecialchars(strip_tags($this->install_type_id));
+            $this->installation_total_charge = htmlspecialchars(strip_tags($this->installation_total_charge));
+            $this->installation_balance = htmlspecialchars(strip_tags($this->installation_balance));
+            $this->installment = htmlspecialchars(strip_tags($this->installment));
+            $this->account_id = htmlspecialchars(strip_tags($this->account_id));
+            $this->installation_status_id = htmlspecialchars(strip_tags($this->installation_status_id));
 
+            // Create Query
+            $query = 'INSERT INTO ' . 
+                    $this->table . '
+                SET
+                    install_type_id = :install_type_id,
+                    installation_total_charge = :installation_total_charge,
+                    installation_balance = :installation_balance,
+                    installment = :installment,
+                    account_id = :account_id,
+                    installation_status_id = :installation_status_id,';
+
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind Data
+            $stmt->bindParam(':install_type_id', $this->install_type_id);
+            $stmt->bindParam(':installation_total_charge', $this->installation_total_charge);
+            $stmt->bindParam(':installation_balance', $this->installation_balance);
+            $stmt->bindParam(':installment', $this->installment);
+            $stmt->bindParam(':account_id', $this->account_id);
+            $stmt->bindParam(':installation_status_id', $this->installation_status_id);
+
+            // Execute Query
+            if ($stmt->execute())
+            {
+                return true;
+            }
+
+            // Print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+
+            return false;
         }
 
         # Get Installation 
         public function read()
         {
+            // Create Query
+            $query = 'SELECT 
+                *
+            FROM
+             ' . $this->table;
+            
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
 
+            $stmt->execute();
+
+            return $stmt;
         }
 
         # Update Installation
