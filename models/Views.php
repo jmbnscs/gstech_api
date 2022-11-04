8dad2c6db19849c702af62ad139efa765542a903
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2022-11-04 08:18:19 +0800

    Added invoice views.

--- models/Views.php
+++ models/Views.php
@@ -25,4 +25,60 @@
 
             return $stmt;
         }
+
+        # Admin Details
+        public function admin()
+        {
+            // Create Query
+            $query = 'SELECT * FROM view_admin_details';
+            
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        # Payment Details
+        public function payment()
+        {
+            // Create Query
+            $query = 'SELECT * FROM view_payment_details';
+            
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        # Prorate Details
+        public function prorate()
+        {
+            // Create Query
+            $query = 'SELECT * FROM view_prorate_details';
+            
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        # Invoice Details
+        public function invoice()
+        {
+            // Create Query
+            $query = 'SELECT * FROM view_invoice_details';
+            
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            $stmt->execute();
+
+            return $stmt;
+        }
     }
