b4699e9b131051232eb1a7848a91355297d19d31
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2023-02-12 22:12:35 +0800

    update

--- api/user_level/delete.php
+++ api/user_level/delete.php
@@ -7,27 +7,32 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 
 include_once '../../config/Database.php';
 include_once '../../models/UserLevel.php';
+include_once '../../models/Restriction.php';
 
 $database = new Database();
 $db = $database->connect();
 
 $user_level = new UserLevel($db);
+$restriction = new Restriction($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
 $user_level->user_id = $data->user_id;
+$restriction->user_id = $data->user_id;
 
 if($user_level->delete()) {
-    echo json_encode(
-    array('success' => true)
-);
+    if($restriction->delete_pages_restriction() && $restriction->delete_buttons_restriction()) {
+        echo json_encode(
+            array('success' => true)
+        );
+    }
 } 
 else {
     echo json_encode(
-    array(
-        'success' => false,
-        'error' => $user_level->error
-    )
-);
+        array(
+            'success' => false,
+            'error' => $user_level->error
+        )
+    );
 }
 
